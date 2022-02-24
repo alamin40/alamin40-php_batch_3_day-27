fe19ba4fa42999ab3421990ef50c853d3ac7bf1b
alamin40 <hossain.78295@gmail.com>
2022-02-24 13:19:00 +0600

    data insert edit delete

---
+++ resources/views/manage-blog.blade.php
@@ -0,0 +1,51 @@
+@extends('master')
+
+@section('title')
+    All Blog
+@endsection
+
+@section('body')
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 mx-auto">
+                    <div class="card">
+                        <div class="card-header font-weight-bold text-center text-white bg-info">All Blog</div>
+                        <div class="card-body">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                <tr class="text-center">
+                                    <th>SL NO</th>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                    <th>Description</th>
+                                    <th>Action</th>
+                                </tr>
+                                @foreach($blogs as $blog)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>{{$blog->id}}</td>
+                                        <td>{{$blog->title}}</td>
+                                        <td>{{$blog->author}}</td>
+                                        <td>{{$blog->description}}</td>
+                                        <td>
+                                            <a href="{{route('edit-blog',['id' => $blog->id])}}" class="btn btn-success btn-sm">
+                                                <i class="fa fa-edit"></i>
+                                            </a>
+                                            <a href="" class="btn btn-danger btn-sm">
+                                                <i class="fa fa-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
